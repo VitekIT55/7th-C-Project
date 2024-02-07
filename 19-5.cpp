1878bd91a12f6e999b81d9917492728c13bbc0ca
VitekIT55 <trakmanic80@gmail.com>
2024-02-07 17:33:12 +0300

    Добавьте файлы проекта.

---
+++ 19-5.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Animal
+{
+public:
+    virtual void Voice()
+    {
+        cout << "Создаю звук";
+    }
+};
+
+class Dog : public Animal
+{
+public:
+    virtual void Voice()
+    {
+        cout << "Гаф!";
+    }
+};
+
+class Cat : public Animal
+{
+public:
+    virtual void Voice()
+    {
+        cout << "Мяу!";
+    }
+};
+
+class Fox : public Animal
+{
+public:
+    virtual void Voice()
+    {
+        cout << "*кричит*";
+    }
+};
+
+int main()
+{
+    system("chcp 1251>nul");
+    Animal* A[4] = {new Animal, new Cat, new Dog, new Fox};
+    for (int i = 0; i < 4; i++)
+    {
+        A[i]->Voice(); 
+        cout << endl;
+    }
+}
